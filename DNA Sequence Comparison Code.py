1dd9ac1ec1e038e0d71c4470f9e8c58c00f311a6
PGMaychell99 <pgreen06@student.bbk.ac.uk>
2025-03-12 19:03:23 +0000

    Completed first function for reading FASTA files

--- DNA Sequence Comparison Code.py
+++ DNA Sequence Comparison Code.py
@@ -1,5 +1,17 @@
-def DNA_Seq_ID(multi_seqfn: str = "dog_breeds.fa", mystery_seqfn: str = "mystery.fa") -> sequence [str]:
-    from Bio import SeqIO
-    from Bio import pairwise2
+from Bio import SeqIO
+from Bio import pairwise2
+
+def DNA_Seq_ID(multi_seqfn: str = "dog_breeds.fa") -> sequence [str]:
+    """ Reads in FASTA file and returns a dictionary of sequences.
+
+
+        Example:"""
+    sequences = {}
+    with open(multi_seqfn, 'r') as f:
+        for record in SeqIO.parse(f, "fasta"):
+            sequences[record.id] = str(record.seq)
+    return sequences
+
+def find_best_match()
 
     
