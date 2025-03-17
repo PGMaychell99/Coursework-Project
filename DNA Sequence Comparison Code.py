d2ee39619ce46cb913ba4a647876a9e8c531ceec
PGMaychell99 <pgreen06@student.bbk.ac.uk>
2025-03-17 20:57:25 +0000

    I have made slight changes to try stop the running indefinitely but not working

--- DNA Sequence Comparison Code.py
+++ DNA Sequence Comparison Code.py
@@ -6,41 +6,56 @@ mystery_seqfn = "./data/project_dog_dna/mystery.fa"
 def DNA_Seq_ID(multi_seqfn: str) -> dict:
     """ Reads in FASTA file and returns a dictionary of sequences.
 
+        Description
 
-        Example:"""
-    sequences = {}
+        Parameters -
+        Returns - 
+
+        Example:
+    """
+    dog_sequences = {}
+
     with open(multi_seqfn, 'r') as f:
         for record in SeqIO.parse(f, "fasta"):
-            sequences[record.id] = str(record.seq) #uses biopython to iterate over each line in the file and store the sequences in a dictionary
-    return sequences
+            dog_sequences[record.id] = str(record.seq) #uses biopython to iterate over each line in the file and store the sequences in a dictionary
+    return dog_sequences
 
-def find_best_match(mystery_seqfn: str, sequences: dict) -> tuple:
+def find_best_match(mystery_seq: str, sequences: dict) -> tuple:
     """ Find the closest sequence in the multi-sequence file to the mystery sequence.
 
         Takes the dictionary from FASTA file conversion dictionary
+
+        Parameters - 
+        Returns - 
+
+        Example:
     """
     best_match = None
     best_score = -1
     best_alignment = None
     best_seq_id = None
-    
-    with open(multi_seqfn, 'r') as f:
-        mystery_seq = "".join([str(record.seq) for record in SeqIO.parse(f, "fasta")])
 
-        for seq_id, seq in sequences.items():
-            alignments = pairwise2.align.globalxx(mystery_seq, seq, score_only = True)
-            score = alignments
+    for seq_id, seq in sequences.items():
+        score = pairwise2.align.globalxx(mystery_seq, seq, score_only = True)
+        
 
-            if score > best_score:
-                best_score = score
-                best_match = seq
-                best_seq_id = seq_id
-                best_alignment = pairwise2.align.globalxx(mystery_seq, seq)[0]
+        if score > best_score:
+            best_score = score
+            best_match = seq
+            best_seq_id = seq_id
+            best_alignment = pairwise2.align.globalxx(mystery_seq, seq)[0]
 
     return best_seq_id, best_match, best_score, best_alignment
 
 def calculate_differences(seq1: str, seq2: str) -> list:
+    """
+    
+    
+    """
     alignments = pairwise2.align.globalxx(seq1, seq2)
+
+    if not alignments:
+        return None, None, []
     aligned_seq1, aligned_seq2, _, _, _ = alignments[0]
 
     differences = []
@@ -52,7 +67,11 @@ def calculate_differences(seq1: str, seq2: str) -> list:
 
 sequences = DNA_Seq_ID(multi_seqfn)
 mystery_seq_dict = DNA_Seq_ID(mystery_seqfn)
-mystery_seq = list(mystery_seq_dict.values())[0]
+
+if mystery_seq_dict:
+    mystery_seq = list(mystery_seq_dict.values())[0]
+else:
+    raise ValueError("Mystery sequence file is empty or incorrectly formatted")
 
 best_seq_id, best_match, best_score, best_alignment = find_best_match(mystery_seq, sequences)
 
